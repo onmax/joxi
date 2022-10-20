46a2c238e7736bf7bfa2884a7021619c905f6fc1
Max <onmax@users.noreply.github.com>
2022-10-20 17:27:49 +0000

    execute submodules only if keys are present

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import { getCoinbaseInfo } from "./coinbase/fetcher.ts";
 import { getBanks } from "./nordigen/fetcher.ts";
 import { syncDatabases } from "./notion/notion.ts";
+import { type Bank } from "./types/custom.d.ts";
 
-const banks = [...await getBanks(), await getCoinbaseInfo()]
+const banks: Bank[] = []
 
-syncDatabases(banks)
+const hasNordigen = Deno.env.get("NORDIGEN_KEY") && Deno.env.get("NORDIGEN_SECRET")
+if (hasNordigen) {
+    banks.push(...await getBanks())
+} else {
+    console.log("⚠️ Ignoring Nordigen because of missing env variables");
+}
+
+const hasCoinbase = Deno.env.get("COINBASE_KEY") && Deno.env.get("COINBASE_SECRET")
+if (hasCoinbase) {
+    banks.push(await getCoinbaseInfo())
+} else {
+    console.log("⚠️ Ignoring Coinbase because of missing env variables");
+}
+
+const hasNotion = Deno.env.get("NOTION_TOKEN") && Deno.env.get("NOTION_DATABASE_ID")
+if (hasNotion) {
+    await syncDatabases(banks)
+} else {
+    console.log("⚠️ Ignoring Notion because of missing env variables");
+}
